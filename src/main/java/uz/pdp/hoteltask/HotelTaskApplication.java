75cf7a52527a70f5dbaff11eb7b34ff818a46db1
muhammad0224 <murtazayevmuhammad@gmail.com>
2021-07-24 09:53:29 +0500

    firstCommit

---
+++ src/main/java/uz/pdp/hoteltask/HotelTaskApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.hoteltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/hoteltask/HotelTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.hoteltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
